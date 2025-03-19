d8028f0c2a0eaa62e814f10de99a861cecbe41e9
God'sgift Uko <godsgiftuko@gmail.com>
2025-03-19 01:45:02 +0100

    Add GitLinks

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,4 +1,6 @@
 export class Config {
     static appName = 'News App';
     static API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+    static FE_GITHUB_URL = 'https://github.com/godsgiftuko/t001-react-nest-mongo-news-app';
+    static BE_GITHUB_URL = 'https://github.com/godsgiftuko/t001-react-nest-mongo-news-api';
 }
